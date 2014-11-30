ab6380f46f9b9fef62c7a57f8a96515d2b4beeac
AHelper <ahelper2@gmail.com>
2014-11-30 04:25:21 -0600

    Updated the header to reflect the new routine.

--- eh/eh.h
+++ eh/eh.h
@@ -12,15 +12,26 @@
 extern "C" {
 #endif
 
+struct EH_Data_t {
+  unsigned int oldVBR;
+  unsigned int VBR100;
+  unsigned int VBR400;
+  unsigned int VBR600;
+  unsigned int pageTableEnd;
+  unsigned int invocationCounter;
+  void (*trapCallback)();
+};
+
 extern volatile unsigned int _EH_Data;
 extern volatile unsigned int _EH_Page_Table;
 
 extern int EH_Install();
 extern int EH_Uninstall();
-const volatile unsigned int* EH_Data = &_EH_Data;
+const volatile  unsigned int* EH_Data = &_EH_Data;
 const volatile unsigned int* EH_Page_Table = &_EH_Page_Table;
 extern int EH_AddPage(int source, int dest, int flags);
 extern int EH_DelPage(int source);
+extern void EH_SetTrapHandler(void (*callback)());
 
 #ifdef __cplusplus
 }
