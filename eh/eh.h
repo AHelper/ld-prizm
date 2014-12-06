4d9fe2ef43a4447cf34bd81dda5827422a65d315
AHelper <ahelper2@gmail.com>
2014-12-06 16:55:02 -0600

    Added a working trap handler. Added MMU TLB cache and related functions. Added MMU override into EH400. Trap handler tested and works. MMU override tested and works.

--- eh/eh.h
+++ eh/eh.h
@@ -19,19 +19,27 @@ struct EH_Data_t {
   unsigned int VBR600;
   unsigned int pageTableEnd;
   unsigned int invocationCounter;
-  void (*trapCallback)();
+  void (*trapCallback)(int);
 };
+
+#define TLB_FLAG_1KB  0b0101101000
+#define TLB_FLAG_4KB  0b0101111000
+#define TLB_FLAG_64KB 0b0111101000
+#define TLB_FLAG_1MB  0b0111111000
 
 extern volatile unsigned int _EH_Data;
 extern volatile unsigned int _EH_Page_Table;
+extern volatile unsigned int _EH_Page_Table_End;
 
 extern int EH_Install();
 extern int EH_Uninstall();
 const volatile  unsigned int* EH_Data = &_EH_Data;
 const volatile unsigned int* EH_Page_Table = &_EH_Page_Table;
-extern int EH_AddPage(int source, int dest, int flags);
-extern int EH_DelPage(int source);
-extern void EH_SetTrapHandler(void (*callback)());
+const volatile unsigned int* EH_Page_Table_End = &_EH_Page_Table_End;
+/// For flags, see bits[10:0] of UTLB data entry.
+extern int EH_AddPage(unsigned int source, unsigned int dest, int flags);
+extern int EH_DelPage(unsigned int source);
+extern void EH_SetTrapHandler(void (*callback)(int code));
 
 #ifdef __cplusplus
 }
